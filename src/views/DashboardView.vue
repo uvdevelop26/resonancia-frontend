d65c9590ecec7ed763effd69de95536134c1d1bc
Beto Villalba <betovill26@gmail.com>
2024-12-30 18:19:27 -0300

    Primer commit

---
+++ src/views/DashboardView.vue
@@ -0,0 +1,13 @@
+<script setup>
+import AppLayout from "@/components/layout/AppLayout.vue";
+</script>
+
+<template>
+  <AppLayout title="Dashboard">
+    <template #content>
+      <div class="w-full h-full overflow-y-auto">
+        <h2 class="text-primary font-bold pb-4">...</h2>
+      </div>
+    </template>
+  </AppLayout>
+</template>
